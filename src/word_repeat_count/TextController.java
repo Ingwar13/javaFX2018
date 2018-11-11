c25aec91e55c8b70c8fcc80b7e871e20b5a2c8ad
Ingwar13 <ingwarflint@gmail.com>
2018-11-11 22:25:09 +0200

    word count #2

--- src/word_repeat_count/TextController.java
+++ src/word_repeat_count/TextController.java
@@ -6,8 +6,20 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 public class TextController {
 
@@ -53,7 +65,39 @@ public class TextController {
 
     @FXML
     private void handleProcess(){
-
+    	File processFile = new File(chooseFileLabel.getText());
+    	File exlusionsFile = new File(exclusionsLabel.getText());
+    	Pattern pattern = Pattern.compile("[a-zA-Z]{3,}");
+    	
+    	FileReader reader;
+    	String line;
+		try {
+			reader = new FileReader(chooseFileLabel.getText());
+			BufferedReader bufferedReader = new BufferedReader(reader);
+			 while ((line = bufferedReader.readLine()) != null) {
+			     for (String word : line.split("[a-zA-Z]{3,}")) {
+			    	 wordMap.put(word, wordMap.get(word)!=null?wordMap.get(word)+1 : 1);
+			     }	     
+			    }
+			 reader.close();
+			 Iterator it = wordMap.entrySet().iterator();
+			    while (it.hasNext()) {
+			        Map.Entry pair = (Map.Entry)it.next();
+			        System.out.println(pair.getKey() + " = " + pair.getValue());
+			    }
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        
+//    	try {
+//    		Files.lines(Paths.get(chooseFileLabel.getText()))
+//    		.forEach(a -> a.split("[a-zA-Z]{3,}"));			
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//    	//processFile.
     }
 
 }
